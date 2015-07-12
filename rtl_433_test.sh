88def10999be95e93ceb7ab3eae2bb458a17a80c
Tommy Vestermark <tov@vestermark.dk>
2015-07-12 07:42:18 +0200

    Added test script
    Very useful for regression testing rtl_433

---
+++ rtl_433_test.sh
@@ -0,0 +1,31 @@
+## Your git project directory
+PRJDIR=~/trysdr
+
+## Test command
+CMD="$PRJDIR/rtl_433/build/src/rtl_433 -r"
+
+## Run though all test data
+$CMD $PRJDIR/rtl_433_tests/tests/acurite/01/VN1TXC_02.data
+$CMD $PRJDIR/rtl_433_tests/tests/ambient_weather/01/f007th.data
+$CMD $PRJDIR/rtl_433_tests/tests/calibeur/RF104/01/gfile001.data
+$CMD $PRJDIR/rtl_433_tests/tests/cardin/01/S466-TX2_01.data
+$CMD $PRJDIR/rtl_433_tests/tests/cardin/01/S466-TX2_02.data
+$CMD $PRJDIR/rtl_433_tests/tests/dsc/01/gfile001.data
+$CMD $PRJDIR/rtl_433_tests/tests/dsc/01/gfile002.data
+$CMD $PRJDIR/rtl_433_tests/tests/fineoffset/01/gfile001.data
+$CMD $PRJDIR/rtl_433_tests/tests/GT-WT-02/gfile003.data
+$CMD $PRJDIR/rtl_433_tests/tests/lacrosse/01/gfile001.data
+$CMD $PRJDIR/rtl_433_tests/tests/lacrosse/01/gfile002.data
+$CMD $PRJDIR/rtl_433_tests/tests/lacrosse/01/gfile006.data
+$CMD $PRJDIR/rtl_433_tests/tests/lacrosse/02/gfile003.data
+$CMD $PRJDIR/rtl_433_tests/tests/lacrosse/02/gfile004.data
+$CMD $PRJDIR/rtl_433_tests/tests/lacrosse/02/gfile010.data
+$CMD $PRJDIR/rtl_433_tests/tests/oregon_scientific/01/BTHGN129.data
+$CMD $PRJDIR/rtl_433_tests/tests/oregon_scientific/01/BTHR968.data
+$CMD $PRJDIR/rtl_433_tests/tests/oregon_scientific/02/gfile003.data
+$CMD $PRJDIR/rtl_433_tests/tests/prologue/01/gfile001.data
+$CMD $PRJDIR/rtl_433_tests/tests/UPM/01/gfile001.data
+$CMD $PRJDIR/rtl_433_tests/tests/UPM/01/gfile002.data
+$CMD $PRJDIR/rtl_433_tests/tests/UPM/01/gfile003.data
+$CMD $PRJDIR/rtl_433_tests/tests/x10/01/record-x10-b1-on-001.data
+$CMD $PRJDIR/rtl_433_tests/tests/x10/01/record-x10-b1-on-002.data
