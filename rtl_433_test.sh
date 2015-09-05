3b6dc127de3c75e9817da0869321b50093f04b90
Stanisław Pitucha <viraptor@gmail.com>
2015-09-06 01:28:49 +1000

    Add samples for the digitech weather station
    Add samples and an emergency mechanism to tolerate bad quality samples.

--- rtl_433_test.sh
+++ rtl_433_test.sh
@@ -1,17 +1,25 @@
 #!/bin/bash
 
 ## Your git project directory
-PRJDIR=~/projects
+PRJDIR=~/Projects
 
 ## Test command
 CMD="$PRJDIR/rtl_433/build/src/rtl_433 -r"
 
 ## Find all data files (sort for consistency between runs)
-DATAFILES=$(find $PRJDIR/rtl_433_tests -iname "*.data" | sort)
+DATAFILES=$(find $PRJDIR/rtl_433_tests -iname "*.data" | grep xc | sort)
 
 ## Run though all test data
 for FILE in $DATAFILES
 do
-	$CMD $FILE
+    # in case of very bad samples, check if they require specific level to pass
+    # use only in emergency
+    LEVEL_FILE="$(dirname "$FILE")/level"
+    if [[ -r "$LEVEL_FILE" ]] ; then
+        LEVEL="-l $(cat "$LEVEL_FILE")"
+    else
+        LEVEL=
+    fi
+	$CMD $FILE $LEVEL
 done
 
