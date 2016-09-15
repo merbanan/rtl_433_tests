cd5e63bb79f987a542fd75a51d909921ba38cbd7
Keijo Kangas <keijo.olavi.kangas@gmail.com>
2016-09-15 12:02:41 +0300

    Updated PHP-script

--- tests/axel/02/temp_graph.php
+++ tests/axel/02/temp_graph.php
@@ -21,28 +21,45 @@ EOF;
 
    $ret = $db->query($sql);
    $array_string_temperature = "";
+   $array_string_timestamp = "";
    while($row = $ret->fetchArray(SQLITE3_ASSOC) ){
-      $array_string_temperature = $array_string_temperature . $row['temperature'] .",";
+     $array_string_temperature[] = $row["temperature"];
+     $array_string_timestamp[] = date('G:i', strtotime(getLocaltime($row["timestamp"])));
    }
-
-   $array_string_temperature = trim($array_string_temperature, ",");
 
    $db->close();
 
+$ydata = array_reverse($array_string_temperature);
+$xdata = array_reverse($array_string_timestamp);
 
+ function getLocaltime($utc_time)
+  {
+    $utc_date = DateTime::createFromFormat(
+                'Y-m-d G:i:s', 
+                $utc_time, 
+                new DateTimeZone('UTC')
+    );
+
+    $local_date = $utc_date;
+    $local_date->setTimeZone(new DateTimeZone('Europe/Helsinki'));
+
+    $timestamp_string = $local_date->format('Y-m-d G:i:s'); 
+    return $timestamp_string;
+  }
 
 require_once('jpgraph/jpgraph.php');
 require_once('jpgraph/jpgraph_line.php');
- //  data
-$ydata = explode(',', $array_string_temperature);
+//require_once('jpgraph/jpgraph_date.php');
+
 // Create the graph. These two calls are always required
 $graph = new Graph(800,600);
 $graph->SetScale('textlin');
 $graph->title->SetFont(FF_FONT2);
 $graph->title->Set("Temperature data last 24 hours");
-$graph->xaxis->title->Set("Temperature data last 24h");
 $graph->yaxis->SetFont(FF_FONT2);
-$graph->xaxis->Hide();
+$graph->xaxis->SetTickLabels($xdata);
+$graph->xaxis->SetTextTickInterval(24,0);
+$graph->xgrid->Show(true);
 // Create the linear plot
 $lineplot=new LinePlot($ydata);
 $lineplot->SetColor('blue');
@@ -52,7 +69,5 @@ $graph->Add($lineplot);
 
 // Display the graph
 $graph->StrokeCSIM("temp_graph.php");
-// $graph->Stroke();
 
 ?>
-
