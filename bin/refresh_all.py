07ce886edb61c4aa50cb5938b2de8e4c00055366
Rick van Schijndel <rol3517@gmail.com>
2020-08-15 16:52:25 +0200

    Add false detection logic and show a summary at the end of a run

--- bin/refresh_all.py
+++ bin/refresh_all.py
@@ -9,17 +9,15 @@ import fnmatch
 import subprocess
 import json
 
-
 def run_rtl433(input_fn, samplerate=None, protocol=None, rtl_433_cmd="rtl_433"):
     """Run rtl_433 and return output."""
-    args = ['-c', '0', '-M', 'newmodel']
+    args = ['-c', '0']
     if protocol:
         args.extend(['-R', str(protocol)])
     if samplerate:
         args.extend(['-s', str(samplerate)])
     args.extend(['-F', 'json', '-r', input_fn])
     cmd = [rtl_433_cmd] + args
-    # print(" ".join(cmd))
     p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     out, err = p.communicate()
     return (out, err, p.returncode)
